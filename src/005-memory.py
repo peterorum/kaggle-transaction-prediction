b1821b1fe05ce281efc0b8a86704b806d31d7b95
Peter Orum <peter@peterorum.com>
2019-03-28 13:14:17 +1100

    rduce memory usage

--- src/005-memory.py
+++ src/005-memory.py
@@ -36,6 +36,69 @@ def timer():
 
     last_time = time()
 
+# Based on this great kernel https://www.kaggle.com/arjanso/reducing-dataframe-memory-size-by-65
+
+
+def reduce_mem_usage(df):
+    start_mem_usg = df.memory_usage().sum() / 1024**2
+
+    NAlist = []  # Keeps track of columns that have missing values filled in.
+
+    for col in df.columns:
+        if df[col].dtype != object:  # Exclude strings
+
+            previous_type = df[col].dtype
+
+            # make variables for Int, max and min
+            IsInt = False
+            mx = df[col].max()
+            mn = df[col].min()
+
+            # Integer does not support NA, therefore, NA needs to be filled
+            if not np.isfinite(df[col]).all():
+                NAlist.append(col)
+                df[col].fillna(mn - 1, inplace=True)
+
+            # test if column can be converted to an integer
+            asint = df[col].fillna(0).astype(np.int64)
+            result = (df[col] - asint)
+            result = result.sum()
+            if result > -0.01 and result < 0.01:
+                IsInt = True
+
+            # Make Integer/unsigned Integer datatypes
+            if IsInt:
+                if mn >= 0:
+                    if mx < 255:
+                        df[col] = df[col].astype(np.uint8)
+                    elif mx < 65535:
+                        df[col] = df[col].astype(np.uint16)
+                    elif mx < 4294967295:
+                        df[col] = df[col].astype(np.uint32)
+                    else:
+                        df[col] = df[col].astype(np.uint64)
+                else:
+                    if mn > np.iinfo(np.int8).min and mx < np.iinfo(np.int8).max:
+                        df[col] = df[col].astype(np.int8)
+                    elif mn > np.iinfo(np.int16).min and mx < np.iinfo(np.int16).max:
+                        df[col] = df[col].astype(np.int16)
+                    elif mn > np.iinfo(np.int32).min and mx < np.iinfo(np.int32).max:
+                        df[col] = df[col].astype(np.int32)
+                    elif mn > np.iinfo(np.int64).min and mx < np.iinfo(np.int64).max:
+                        df[col] = df[col].astype(np.int64)
+
+            # Make float datatypes 32 bit
+            else:
+                df[col] = df[col].astype(np.float32)
+
+            print(f"{col} before: {previous_type}, after: {df[col].dtype}")
+
+    mem_usg = df.memory_usage().sum() / 1024**2
+
+    print(f"Memory usage is: {mem_usg}MB, {100 * mem_usg / start_mem_usg:.1f}% of the initial size")
+
+    return df, NAlist
+
 
 def evaluate(train, test, unique_id, target):
 
@@ -476,6 +539,9 @@ def run():
 
     train = pd.read_csv(f'../input/{train_file}.csv{zipext}')
     test = pd.read_csv(f'../input/test.csv{zipext}')
+
+    train, _ = reduce_mem_usage(train)
+    test, _ = reduce_mem_usage(test)
 
     train, test = get_many_missing_values(train, test, unique_id, target)
 
